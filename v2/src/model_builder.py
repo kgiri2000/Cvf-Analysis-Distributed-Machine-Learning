9a773f298f8cadc0cec964d30248d3b86f42b093
kgiri2000 <kgiris2o57@gmail.com>
2025-10-31 00:47:15 -0600

    Distributed dataset error

--- v2/src/model_builder.py
+++ v2/src/model_builder.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 
-def build_feed_forward_model(input_dim, output_dim, learning_rate=0.0001):
-    model = tf.keras.Sequential([
-        #input_dim = X_train.shape[1]
-        tf.keras.layers.Dense(64, activation="relu", input_shape=(input_dim,)),
-        tf.keras.layers.Dense(128,activation='relu'),
-        tf.keras.layers.Dense(64, activation="relu"),
-        #Output layer
-        tf.keras.layers.Dense(1) 
-    ])
+def build_feed_forward_model(input_dim, output_dim, learning_rate=0.001):
+    inputs = tf.keras.Input(shape=(input_dim,), dtype=tf.float32)
+    x = tf.keras.layers.Dense(64, activation='relu')(inputs)
+    x = tf.keras.layers.Dense(32, activation='relu')(x)
+    outputs = tf.keras.layers.Dense(output_dim, activation='linear')(x)
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate)
-    model.compile(optimizer= optimizer, loss ='mse', metrics=['mae'])
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+        loss='mse',
+        metrics=['mae']
+    )
     return model
